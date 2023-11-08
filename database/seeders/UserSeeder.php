bd1e7fe3e7c6a888e2de95df9af1bb3813cd070d
Adi Putra <m.a.syaputra.94@gmail.com>
2023-11-09 00:32:36 +0700

    remove user kepala seeder

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -39,18 +39,18 @@ class UserSeeder extends Seeder
                 'identify' => '1234567892',
                 'email_verified_at' => now(),
             ],
-            [
-                'name' => 'Kepala',
-                'email' => 'Kepala@testing.com',
-                'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
-                'slug' => 'testing-kepala',
-                'telp' => '08978309012',
-                'role' => 'Kepala',
-                'birthdate' => '2001-07-22',
-                'gender' => 'Laki-laki',
-                'identify' => '1234567893',
-                'email_verified_at' => now(),
-            ],
+            // [
+            //     'name' => 'Kepala',
+            //     'email' => 'Kepala@testing.com',
+            //     'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
+            //     'slug' => 'testing-kepala',
+            //     'telp' => '08978309012',
+            //     'role' => 'Kepala',
+            //     'birthdate' => '2001-07-22',
+            //     'gender' => 'Laki-laki',
+            //     'identify' => '1234567893',
+            //     'email_verified_at' => now(),
+            // ],
         ];
         foreach ($users as $user) {
             User::create($user);
